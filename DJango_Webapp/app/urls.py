b2806b4689cb3405a31d83ad7b63b35ba0e66212
ramukodandapuram <ramukodandapuram@gmail.com>
2018-03-06 00:42:31 +0530

    DjangoWebapp First Commnit

---
+++ DJango_Webapp/app/urls.py
@@ -0,0 +1,12 @@
+from  django.conf.urls import url
+from . import  views
+
+
+urlpatterns=[
+
+
+		url(r'^$',  views.HomePageView.as_view()),
+		url(r'^about/$',views.AboutPageView.as_view()),
+
+
+]
